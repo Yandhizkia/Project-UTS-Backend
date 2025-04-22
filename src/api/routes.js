81e7f60efc398982054e865e2c0a15658e96b8c3
RichardV-H <godof7sea@gmail.com>
2025-04-22 22:12:04 +0700

    pull

--- src/api/routes.js
+++ src/api/routes.js
@@ -2,16 +2,12 @@ const express = require('express');
 
 const luffyGears = require('./components/luffy-gears/luffy-gears-route');
 const dials = require('./components/dials/dials-route');
-const characters = require('./components/characters/characters-route');
-const crews = require('./components/crews/crews-route');
 
 module.exports = () => {
   const app = express.Router();
 
   luffyGears(app);
   dials(app);
-  characters(app);
-  crews(app);
 
   return app;
 };
